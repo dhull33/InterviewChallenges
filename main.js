9a582d4f077da78706a4bc0691cd451f7aeb8955
David Hull <david.hull1@utexas.edu>
2018-04-08 18:30:35 -0500

    Added smallNum function

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
+/*2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+*What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+function smallNUm(){
+    var flag = '';
+    var n =1;
+    var largest_num = 20;
+    var num = 2520;
 
+    while (flag == ''){
+        if(num % n != 0){
+            num += largest_num;
+            n=1;
+        }
+        else if(num % n == 0){
+            n+=1;
+        }
 
-var chairs = Array(1018);
-chairs.fill(1);
-
-for(var i = 0; i < chairs.length; i ++){
-    if (chairs.indexOf(1, i) == i){
-        chairs[i] = 0;
+        if (n == largest_num && num % n == 0){
+            flag = 'Stop'
+        }
     }
 
+    return num
+};
 
-}
